6f656875dfabdca5ffcde75ab78fbe68ba8d387e
banboy38 <nabovtl@gmail.com>
2022-05-18 18:22:55 +0530

    gg

--- dev/blah.py
+++ dev/blah.py
@@ -19,13 +19,13 @@ def parse(json_string):
     """
     return json.loads(json_string)
 
-print parse('[{"faceRectangle": {"height": 53, "left": 161, "top": 118, "width": 53},'
+print(parse('[{"faceRectangle": {"height": 53, "left": 161, "top": 118, "width": 53},'
             '"scores": {"anger": 0.0021335755, "contempt": 0.0148000093,  "disgust": 0.000458147872, '
             '"fear": 8.82706954e-05, "happiness": 0.0007227033, "neutral": 0.9745141, '
-            '"sadness": 0.00594675448, ''"surprise": 0.00133642368}}]')
+            '"sadness": 0.00594675448, ''"surprise": 0.00133642368}}]'))
 # output: [{u'faceRectangle': {u'width': 53, u'top': 118, u'left': 161, u'height': 53}, u'scores': {u'sadness': 0.00594675448, u'neutral': 0.9745141, u'contempt': 0.0148000093, u'disgust': 0.000458147872, u'anger': 0.0021335755, u'surprise': 0.00133642368, u'fear': 8.82706954e-05, u'happiness': 0.0007227033}}]
 
-print parse('[]')
+print(parse('[]'))
 # output: []
 
 
